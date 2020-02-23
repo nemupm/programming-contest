4931118891726cf3587b745faa4fc7618c93ca72
noriyuki-matsuoka <nemupm@gmail.com>
2020-02-23 17:00:13 +0900

    added floor ceil

--- library/lib.hpp
+++ library/lib.hpp
@@ -14,3 +14,15 @@ long long getGcd(long long a, long long b) {
 long long getLcm(long long a, long long b) {
     return a * b / getGcd(a, b);
 }
+
+ll floor(ll a, ll b) {
+    if (abs(a) % abs(b) == 0) return a / b;
+    if ((a > 0 && b > 0) || (a < 0 && b < 0)) return a / b;
+    else return a / b - 1;
+}
+
+ll ceil(ll a, ll b) {
+    if (abs(a) % abs(b) == 0) return a / b;
+    if ((a > 0 && b > 0) || (a < 0 && b < 0)) return a / b + 1;
+    else return a / b;
+}
