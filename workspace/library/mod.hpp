e9463ba73602615cb7e9039807aa0fed8bde6873
noriyuki-matsuoka <nemupm@gmail.com>
2020-02-23 15:06:28 +0900

    update

--- workspace/library/mod.hpp
+++ workspace/library/mod.hpp
@@ -25,19 +25,51 @@ long long modFactorial(long long a, long long p) {
     return fact;
 }
 
-long long combination(long long n, long long k, long long p) {
-    // calculates C(n,k) mod p (assuming p is prime).
+struct combination {
+    void preprocess(long long max_n, long long p) {
+        factorials_ = vector<ll>(max_n+1, 1);
+        inverse_factorials_ = vector<ll>(max_n+1, 1);
+        for (long long i=1; i<max_n+1; i++) {
+            factorials_[i] = factorials_[i-1] * i;
+            factorials_[i] %= p;
+        }
+        inverse_factorials_[max_n] = modPow(factorials_[max_n],  p-2, p);
+        for (long long i=max_n-1; i>=1; i--) {
+            inverse_factorials_[i] = inverse_factorials_[i+1] * (i+1);
+            inverse_factorials_[i] %= MOD;
+        }
 
-    long long numerator = 1; // n * (n-1) * ... * (n-k+1)
-    for (int i=0; i<k; i++) {
-        numerator = (numerator * (n-i) ) % p;
+        _is_preprocessed = true;
     }
 
-    long long denominator = 1; // k!
-    for (int i=1; i<=k; i++) {
-        denominator = (denominator * i) % p;
+    long long combination_with_preprocessing(long long n, long long k, long long p) {
+        if (!_is_preprocessed) assert(false);
+
+        long long result = factorials_[n];
+        result *= inverse_factorials_[k];
+        result %= p;
+        result *= inverse_factorials_[n-k];
+        result %= p;
+        return result;
     }
 
-    // numerator / denominator mod p.
-    return ( numerator* modInverse(denominator,p) ) % p;
-}
+    static long long combination_without_preprocessing(long long n, long long k, long long p) {
+        // calculates C(n,k) mod p (assuming p is prime).
+        long long numerator = 1; // n * (n-1) * ... * (n-k+1)
+        for (int i=0; i<k; i++) {
+            numerator = (numerator * (n-i) ) % p;
+        }
+
+        long long denominator = 1; // k!
+        for (int i=1; i<=k; i++) {
+            denominator = (denominator * i) % p;
+        }
+        // numerator / denominator mod p.
+        return ( numerator* modInverse(denominator,p) ) % p;
+    }
+
+private:
+    vector<ll> factorials_, inverse_factorials_;
+    bool _is_preprocessed = false;
+};
+
