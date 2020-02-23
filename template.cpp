99232fbf36eeaa8e93b9ca767753deab0638017f
noriyuki-matsuoka <nemupm@gmail.com>
2020-02-23 15:40:55 +0900

    atcoder-tools

---
+++ template.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i,x,y) for(ll i=(x);i<(y);i++)
+#define rrep(i,x,y) for(ll i=(ll)(y)-1;i>=(x);i--)
+#define all(x) (x).begin(),(x).end()
+#define dump(x)  cerr << #x << " = " << (x) << endl
+#define debug(x) cerr << #x << " = " << (x) << " (L" << __LINE__ << ")" << " " << __FILE__ << endl
+//#define int long long
+
+typedef long long ll;
+const ll MOD = 1e9 + 7;
+
+{% if prediction_success %}
+void solve({{ formal_arguments }}){
+}
+{% endif %}
+
+signed main(){
+    {% if prediction_success %}
+    {{input_part}}
+    solve({{ actual_arguments }});
+    {% else %}
+    // Failed to predict input format
+    {% endif %}
+    return 0;
+}
