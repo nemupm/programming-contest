9895ee7d06ef73553cf3d25368662a331f038882
nemupm <nemupm@gmail.com>
2020-05-21 12:52:36 +0900

    added modint from snuke

--- library/mod.hpp
+++ library/mod.hpp
@@ -25,4 +25,40 @@ namespace mod {
         }
         return fact;
     }
+
+    long long MOD = 1e9+7;
+
+    struct mint {
+        long long x;
+        mint(long long x = 0) : x((x%MOD+MOD)%MOD){}
+        mint operator-() const { return mint(-x);}
+        mint& operator+=(const mint a) {
+            if ((x += a.x) >= MOD) x -= MOD;
+            return *this;
+        }
+        mint& operator-=(const mint a) {
+            if ((x += MOD-a.x) >= MOD) x -= MOD;
+            return *this;
+        }
+        mint& operator*=(const mint a) {
+            (x *= a.x) %= MOD; return *this;
+        }
+        mint operator+(const mint a) const { return mint(*this) += a;}
+        mint operator-(const mint a) const { return mint(*this) -= a;}
+        mint operator*(const mint a) const { return mint(*this) *= a;}
+        mint pow(long long t) const {
+            if (!t) return 1;
+            mint a = pow(t>>1);
+            a *= a;
+            if (t&1) a *= *this;
+            return a;
+        }
+
+        // for prime mod
+        mint inv() const { return pow(MOD-2);}
+        mint& operator/=(const mint a) { return *this *= a.inv();}
+        mint operator/(const mint a) const { return mint(*this) /= a;}
+    };
+    std::istream& operator>>(std::istream& is, const mint& a) { return is >> a.x;}
+    std::ostream& operator<<(std::ostream& os, const mint& a) { return os << a.x;}
 }
